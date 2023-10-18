cb02da5b2473670bf26b1cb4ad01304f362d418e
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-18 11:32:05 -0300

    Archivo para las constantes agregado

---
+++ components/constants/index.js
@@ -0,0 +1,27 @@
+export const menuLinks = [
+    { texto: "Sobre nosotros", ruta: "/nosotros" },
+    { texto: "Disciplinas", ruta: "/disciplinas" },
+    { texto: "Entrenadores", ruta: "/entrenadores" },
+  ];
+
+export const footerLinks = [
+  
+]
+
+export const sobreNosotrosInfo = [
+  {
+    imagen: "/assets/aboutUs.webp",
+    titulo: "Nosotros",
+    texto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates aperiam totam mollitia temporibus nostrum unde hic quos aut quo sit doloribus eius ullam nisi placeat accusantium enim in error dicta, magni debitis perferendis exercitationem! Alias, quae nisi tenetur a culpa perferendis eius eligendi facilis facere nemo harum doloribus ducimus autem amet? Vitae, aliquid expedita? Doloribus itaque suscipit distinctio dolorum quae. Repudiandae eveniet illo facere nihil deleniti esse, veniam earum repellendus cum dicta nemo necessitatibus quidem accusamus deserunt.",
+  },
+  {
+    imagen: "/assets/aboutUs.webp",
+    titulo: "Por que elegirnos",
+    texto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates aperiam totam mollitia temporibus nostrum unde hic quos aut quo sit doloribus eius ullam nisi placeat accusantium enim in error dicta, magni debitis perferendis exercitationem! Alias, quae nisi tenetur a culpa perferendis eius eligendi facilis facere nemo harum doloribus ducimus autem amet? Vitae, aliquid expedita? Doloribus itaque suscipit distinctio dolorum quae. Repudiandae eveniet illo facere nihil deleniti esse, veniam earum repellendus cum dicta nemo necessitatibus quidem accusamus deserunt. ",
+  },
+  {
+    imagen: "/assets/aboutUs.webp",
+    titulo: "Nuestro equipo",
+    texto: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus voluptates aperiam totam mollitia temporibus nostrum unde hic quos aut quo sit doloribus eius ullam nisi placeat accusantium enim in error dicta, magni debitis perferendis exercitationem! Alias, quae nisi tenetur a culpa perferendis eius eligendi facilis facere nemo harum doloribus ducimus autem amet? Vitae, aliquid expedita? Doloribus itaque suscipit distinctio dolorum quae. Repudiandae eveniet illo facere nihil deleniti esse, veniam earum repellendus cum dicta nemo necessitatibus quidem accusamus deserunt.",
+  },
+]
