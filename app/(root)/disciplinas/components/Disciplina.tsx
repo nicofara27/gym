ea122a92c1b19fdd68ee191a8405637835f7bb50
Nicolas Fara <nicolas.fara.romero@gmail.com>
2023-10-24 18:21:57 -0300

    Cambio en la posicion y el fondo del texto

--- app/(root)/disciplinas/components/Disciplina.tsx
+++ app/(root)/disciplinas/components/Disciplina.tsx
@@ -9,8 +9,8 @@ export default function Disciplina() {
     <article className="flex flex-col">
         <div className="relative w-full md:h-[20vh] lg:h-[35vh] group">
             <Link href={`/disciplina/${nombre}`}>
-            <Image className="group-hover:grayscale" src={"/assets/aboutTrainer.jpeg"} alt="disciplina" fill/>
-            <h4 className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-4xl font-bold tracking-wider text-lime-600 hidden group-hover:block">Crossfit</h4>
+            <Image className="grayscale group-hover:grayscale-0 duration-200" src={"/assets/aboutTrainer.jpeg"} alt="disciplina" fill/>
+            <h4 className="absolute top-[70%] left-[50%] translate-x-[-50%] p-2 bg-[#111111cf] duration-200 rounded-full text-4xl font-bold tracking-wider text-lime-600">Crossfit</h4>
             </Link>
         </div>
     </article>
